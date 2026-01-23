f7226c99d566a02065842c84a54c26d61081a879
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-01-23 17:48:13 +0100

    Add support for compression level

--- ext/misc/sqlar.c
+++ ext/misc/sqlar.c
@@ -20,7 +20,7 @@ SQLITE_EXTENSION_INIT1
 #include <assert.h>
 
 /*
-** Implementation of the "sqlar_compress(X)" SQL function.
+** Implementation of the "sqlar_compress(X,#)" SQL function.
 **
 ** If the type of X is SQLITE_BLOB, and compressing that blob using
 ** zlib utility function compress() yields a smaller blob, return the
@@ -39,21 +39,26 @@ static void sqlarCompressFunc(
   int argc,
   sqlite3_value **argv
 ){
-  assert( argc==1 );
+  assert( argc==2 );
   if( sqlite3_value_type(argv[0])==SQLITE_BLOB ){
     const Bytef *pData = sqlite3_value_blob(argv[0]);
     uLong nData = sqlite3_value_bytes(argv[0]);
+    int lvl = sqlite3_value_int(argv[1]);
     uLongf nOut = compressBound(nData);
     Bytef *pOut;
+    if( lvl == -1 )
+      lvl = Z_DEFAULT_COMPRESSION;
+    else if( lvl > Z_BEST_COMPRESSION )
+      lvl = Z_BEST_COMPRESSION;
 
     pOut = (Bytef*)sqlite3_malloc(nOut);
     if( pOut==0 ){
       sqlite3_result_error_nomem(context);
       return;
     }else{
-      if( Z_OK!=compress(pOut, &nOut, pData, nData) ){
+      if( Z_OK!=compress2(pOut, &nOut, pData, nData, lvl) ){
         sqlite3_result_error(context, "error in compress()", -1);
-      }else if( nOut<nData ){
+      }else if( nOut<nData && lvl != 0 ){
         sqlite3_result_blob(context, pOut, nOut, SQLITE_TRANSIENT);
       }else{
         sqlite3_result_value(context, argv[0]);
@@ -114,7 +119,7 @@ int sqlite3_sqlar_init(
   int rc = SQLITE_OK;
   SQLITE_EXTENSION_INIT2(pApi);
   (void)pzErrMsg;  /* Unused parameter */
-  rc = sqlite3_create_function(db, "sqlar_compress", 1, 
+  rc = sqlite3_create_function(db, "sqlar_compress", 2,
                                SQLITE_UTF8|SQLITE_INNOCUOUS, 0,
                                sqlarCompressFunc, 0, 0);
   if( rc==SQLITE_OK ){
